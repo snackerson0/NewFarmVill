cca1f5acacdde14ad588253c3b1711fd562015fa
Alex Raymondo <snackerson0@hotmail.com>
2018-10-04 17:47:49 -0400

    first commit

---
+++ NewFarmVill/Assets/Resources.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resources : MonoBehaviour
+{
+    
+    public float wood, stone, food;
+    [Header("UI Reference")]
+    public Text resourcesText;
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+	    resourcesText.text = "Wood " + wood + "\nStone " + stone + "\nFood " + food; 
+	}
+}
